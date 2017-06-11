9d1b0d0acd6634fc037c298896d9360e10437101
moullos <panayiotis.moullotos@gmail.com>
2017-06-11 14:53:03 +0100

    Initial Commit

---
+++ IdP/__init__.py
@@ -0,0 +1,1 @@
+from .IdP import app
---
+++ IdP/models.py
@@ -0,0 +1,134 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from datetime import datetime, timedelta
+from werkzeug import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True, index=True,
+                         nullable=False)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    pwdhash = db.Column(db.String(54), nullable=False)
+    email = db.Column(db.String(120),unique=True, nullable=True)
+
+    def __init__(self, username, firstname, lastname, email, password):
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email.lower()
+        self.set_password(password)
+    
+    def set_password(self, password):
+        self.pwdhash = generate_password_hash(password)
+
+    @property
+    def check_password(self, password):
+        return check_password_hash(self.pwdhash, password)
+
+
+class Client(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    # human readable name
+    name = db.Column(db.String(40))
+    client_id = db.Column(db.String(40), primary_key=True)
+    client_secret = db.Column(db.String(55), unique=True, index=True,
+                              nullable=False)
+    client_type = db.Column(db.String(20), default='public')
+    _redirect_uris = db.Column(db.Text)
+    default_scope = db.Column(db.Text, default='email address')
+
+    @property
+    def user(self):
+        return User.query.get(1)
+
+    @property
+    def redirect_uris(self):
+        if self._redirect_uris:
+            return self._redirect_uris.split()
+        return []
+
+    @property
+    def default_redirect_uri(self):
+        return self.redirect_uris[0]
+
+    @property
+    def default_scopes(self):
+        if self.default_scope:
+            return self.default_scope.split()
+        return []
+
+    @property
+    def allowed_grant_types(self):
+        return ['authorization_code', 'password', 'client_credentials',
+                'refresh_token']
+
+
+class Grant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE')
+    )
+    user = relationship('User')
+
+    client_id = db.Column(
+        db.String(40), db.ForeignKey('client.client_id', ondelete='CASCADE'),
+        nullable=False,
+    )
+    client = relationship('Client')
+    code = db.Column(db.String(255), index=True, nullable=False)
+
+    redirect_uri = db.Column(db.String(255))
+    scope = db.Column(db.Text)
+    expires = db.Column(db.DateTime)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    @property
+    def scopes(self):
+        if self.scope:
+            return self.scope.split()
+        return None
+
+
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(
+        db.String(40), db.ForeignKey('client.client_id', ondelete='CASCADE'),
+        nullable=False,
+    )
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE')
+    )
+    user = relationship('User')
+    client = relationship('Client')
+    token_type = db.Column(db.String(40))
+    access_token = db.Column(db.String(255))
+    refresh_token = db.Column(db.String(255))
+    expires = db.Column(db.DateTime)
+    scope = db.Column(db.Text)
+
+    def __init__(self, **kwargs):
+        expires_in = kwargs.pop('expires_in', None)
+        if expires_in is not None:
+            self.expires = datetime.utcnow() + timedelta(seconds=expires_in)
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    @property
+    def scopes(self):
+        if self.scope:
+            return self.scope.split()
+        return []
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
