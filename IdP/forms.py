4377898bd4c13980ccaf0dde79159d9e06142145
moullos <panayiotis.moullotos@gmail.com>
2017-06-13 21:02:54 +0100

    Added login form

--- IdP/forms.py
+++ IdP/forms.py
@@ -13,24 +13,6 @@ class SignupForm(Form):
     ])
     confirm = PasswordField('Repeat Password')
 
-"""
-class AuthorizationForm(Form):
-  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
-  password = PasswordField('Password', [validators.Required("Please enter a password.")])
-  submit = SubmitField("Authorize")
-   
-  def __init__(self, *args, **kwargs):
-    Form.__init__(self, *args, **kwargs)
- 
-  def validate(self):
-    if not Form.validate(self):
-      return False
-     
-    user = User.query.filter_by(email = self.email.data.lower()).first()
-    if user and user.check_password(self.password.data):
-      return True
-    else:
-      self.email.errors.append("Invalid e-mail or password")
-      return False
-
-"""
+class LoginForm(Form):
+    email = StringField('Email', [validators.Email("Please enter a valid email")])
+    password = PasswordField('Password', [validators.DataRequired()])
