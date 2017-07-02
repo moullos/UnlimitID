a5901714e9540b75202fd41b3374f32d9c8c54dc
moullos <panayiotis.moullotos@gmail.com>
2017-07-02 17:09:22 +0100

    Credential datetime is now in days

--- IdP/config.py
+++ IdP/config.py
@@ -1,9 +1,6 @@
-import os
-from IdP import app
-PSEUDONYM_ENTRY_LIFETIME = 300
 # The keys a user exposes to a client
 ANONYMOUS_KEYS = ['name', 'gender', 'zoneinfo', 'birthdate' ]
-# Credential Lifetime. Default is two weeks
-CREDENTIAL_LIFETIME = 1209600
-
-CRYPTO_DIR = os.path.join(app.instance_path,'crypto')
+# Credential Lifetime in days. Default is two weeks
+CREDENTIAL_LIFETIME = 14
+# Crypto Directory
+CRYPTO_DIR = 'crypto'
