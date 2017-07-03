18c299a599d58d415cdb6b21ee4ec39d1ee5f6da
moullos <panayiotis.moullotos@gmail.com>
2017-07-03 17:20:30 +0100

    Added requirements

--- IdP/setup.py
+++ IdP/setup.py
@@ -6,8 +6,11 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask==0.11.1',
-        'Flask-OAuthlib==0.9.4',
-        'Flask-SQLAlchemy==2.1'
+        'Flask-OAuthlib',
+        'Flask-SQLAlchemy==2.1',
+        'petlib',
+        'wtforms',
+        'Flask-WTF==0.14'
     ],
     dependency_links=['https://github.com/gdanezis/petlib.git/tarball/master']
 )
--- User/User/__init__.py
+++ User/User/__init__.py
@@ -1,12 +1,7 @@
 from flask import Flask
-from amacscreds.cred_user import CredentialUser
-
-app = Flask(__name__)
-from config import  CRYPTO_DIR, INFO_URL
-cs = CredentialUser(CRYPTO_DIR, info_url = INFO_URL)
-    
 import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] ='true'
+app = Flask(__name__)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
 app.debug = True
 app.secret_key = 'development'
 import User.views
