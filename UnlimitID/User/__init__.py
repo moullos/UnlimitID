254eca81dbb5cc3b9fd4b4f4c798ad35e3316d6f
moullos <panayiotis.moullotos@gmail.com>
2017-07-18 13:35:01 +0100

    Minor bug fixes

--- UnlimitID/User/__init__.py
+++ UnlimitID/User/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from .views import setUpViews
-import os
 
 
 def create_app(crypto_dir, idp_url=None, params=None, ipub=None, user_cs=False):
     app = Flask(__name__)
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
-    credential_url = idp_url + 'unlimitID/credential'
-    info_url = idp_url + 'unlimitID/.well-known/info'
+    credential_url = idp_url + '/unlimitID/credential'
+    info_url = idp_url + '/unlimitID/info'
     app, cs = setUpViews(app, crypto_dir, credential_url,
                          info_url=info_url, params=params, ipub=ipub)
     if user_cs is True:
