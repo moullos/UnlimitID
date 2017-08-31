1efc853d46173672de5330d871a4644e97a87775
moullos <panayiotis.moullotos@gmail.com>
2017-08-31 17:01:04 +0100

    Some extra comments

--- config_IdP.py
+++ config_IdP.py
@@ -9,11 +9,11 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR,'IdP.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DATABASES_CONNECT_OPTIONS = {}
 
-# Enable CSRF mitigation
+# Disable CSRF mitigation for testing. Enable in a real deployment
 WTF_CSRF_ENABLED = False
 WTF_CSRF_SESSION_KEY = 'secret'
 
-# Secret Key for signing cookies
+# Secret Key for signing cookies. Stronger key in real deployment
 SECRET_KEY = 'secret'
 
 # Folder for storing cryptographic files
